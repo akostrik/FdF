21576e2598997dc337785196ba2020088d550357
akostrik <internet124816@gmail.com>
2023-07-23 09:11:02 +0200

    -

--- libft/libft.h
+++ libft/libft.h
@@ -34,7 +34,7 @@ void		*ft_memset(void *s, int c, size_t n);
 void		ft_bzero(void *s, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
-size_t		ft_strlcpy(char git checkout stable*dst, const char *src, size_t n);
+size_t		ft_strlcpy(char *dst, const char *src, size_t n);
 size_t		ft_strlcat(char *dst, const char *src, size_t n);
 int			ft_toupper(int c);
 int			ft_tolower(int c);
